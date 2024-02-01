fe86c9952f827a9da1cefe3d2eb79f16172147ed
Santoso Wijaya <swijaya@google.com>
2024-02-01 19:22:12 +0000

    Add more set up procedures in `install.sh` script.
    Per GitHub docs, if `install.sh` or its like is present, then the
    dotfiles in this repo will *not* be automatically copied over.
    
    So, we have to explicitly handle them within the install script.
    
    More to come in subsequent commits, after I validate the changes
    here.

--- install.sh
+++ install.sh
@@ -1,2 +1,26 @@
+#!/bin/bash
+
+apt-get update -q && apt-get upgrade
+apt-get install -qy --no-install-recommends \
+  build-essential \
+  curl \
+  wget \
+  git \
+  vim \
+  ca-certificates \
+  gnupg2 \
+apt-get clean
+
+
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
   curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+
+pushd ~
+git clone --recurse-submodules https://github.com/santoso-wijaya/dotfiles.git
+find $(pwd -P)/.vim/bundle -type d -depth 1  -execdir \
+  ln -s $(pwd -P)/.vim/bundle/{} ${HOME}/.vim/bundle/{} \;
+
+echo -e "dotfiles have been set up by:\n" > ~/dotfiles.txt
+echo -e "https://github.com/santoso-wijaya/dotfiles/blob/main/install.sh" >> ~/dotfiles.txt
+
