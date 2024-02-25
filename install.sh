08e3b0fa2a69cb58da4946b446b166ab06f847bb
swijaya <swijaya@google.com>
2024-02-25 19:53:19 +0000

    Update `install.sh` script to bootstrap vim-plug

--- install.sh
+++ install.sh
@@ -18,10 +18,11 @@ git config --global alias.adog "log --all --decorate --oneline --graph"
 git config --global alias.xl "log --graph --abbrev=commit --decorate --date=relative --all"
 
 
-mkdir -p ~/.vim/autoload ~/.vim/bundle && \
-  curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
-
+# Fetch and place some plugins for pathogen to consume
 pushd ~
 git clone --recurse-submodules https://github.com/santoso-wijaya/dotfiles.git
 cd dotfiles
@@ -29,6 +30,11 @@ find $(pwd -P)/.vim/bundle -maxdepth 1 -type d -execdir \
   ln -s $(pwd -P)/.vim/bundle/{} ${HOME}/.vim/bundle/{} \;
 ln -s $(pwd -P)/.vimrc ${HOME}/.vimrc
 popd
+
+
+# Install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
 echo -e "dotfiles have been set up by:" > ~/dotfiles.txt
